32b69603a02f7c76a20979670386584fdf4acbe8
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 16:58:21 +0800

    Changed the key to respective key values.

--- DECRYPTION-PROJECT.py
+++ DECRYPTION-PROJECT.py
@@ -6,17 +6,26 @@ import string
 # Make a list for key.
 key = " " + string.punctuation + string.digits + string.ascii_letters
 key = list(key)
-print (key)
+
 # Make a duplicate list for keyvalue.
 key_values = " " + string.punctuation + string.digits + string.ascii_letters
 key_values = list(key_values)
-print (key_values)
 
 # Change every ! to "u".
+key_values [1] = "u"
+
 # Change every # to "i".
+key_values [3] = "i"
+
 # Change every & to "e".
+key_values [6] = "e"
+
 # Change every * to "a".
+key_values [10] = "a"
+
 # Change every + to "o".
+key_values [11] = "o"
+
 # Ask user for input message to decrypt.
 # Print the decryptyed message.
 
