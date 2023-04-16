abd98a3b1ab7e5fc97c33498ce256995f04ff463
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 16:51:47 +0800

    Generated a list for key and key values.

--- DECRYPTION-PROJECT.py
+++ DECRYPTION-PROJECT.py
@@ -1,8 +1,17 @@
 # DECRYPTION
 
 # Import string funstion to generate a list.
+import string
+
 # Make a list for key.
+key = " " + string.punctuation + string.digits + string.ascii_letters
+key = list(key)
+print (key)
 # Make a duplicate list for keyvalue.
+key_values = " " + string.punctuation + string.digits + string.ascii_letters
+key_values = list(key_values)
+print (key_values)
+
 # Change every ! to "u".
 # Change every # to "i".
 # Change every & to "e".
