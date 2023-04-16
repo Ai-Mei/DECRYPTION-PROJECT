830af6792c4451faf7a63994588d8656ccce81d8
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-16 16:43:10 +0800

    Psuedocode

--- DECRYPTION-PROJECT.py
+++ DECRYPTION-PROJECT.py
@@ -1,1 +1,14 @@
+# DECRYPTION
+
+# Import string funstion to generate a list.
+# Make a list for key.
+# Make a duplicate list for keyvalue.
+# Change every ! to "u".
+# Change every # to "i".
+# Change every & to "e".
+# Change every * to "a".
+# Change every + to "o".
+# Ask user for input message to decrypt.
+# Print the decryptyed message.
+
 
